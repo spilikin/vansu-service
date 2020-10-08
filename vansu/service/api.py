613aa6e87d5fa3952395358700198a0717aba2a7
spilikin <108782+spilikin@users.noreply.github.com>
2020-10-08 19:10:25 +0200

    Add FHIR dependency

--- vansu/service/api.py
+++ vansu/service/api.py
@@ -1,4 +1,7 @@
+from fhir.resources.organization import Organization
+from fhir.resources.address import Address
 from fastapi import APIRouter
+
 from . import __version__
 
 api = APIRouter()
@@ -6,3 +9,15 @@ api = APIRouter()
 @api.get("/info")
 async def root():
     return {"version": __version__}
+
+@api.get("/Organization")
+async def get_organization():
+    org = Organization()
+    org.id = "f001"
+    org.active = True
+    org.name = "Globex"
+    org.address = list()
+    address = Address()
+    address.country = "Germany"
+    org.address.append(address)
+    return org.dict()
